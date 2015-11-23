1ab645a7f0ec71167dff98b19149d6e04994fdf9
razibdeb <razibdeb@gmail.com>
2015-11-23 22:11:35 +0600

    Initial Commit

---
+++ DatabaseHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DatabaseHelper
+//
+//  Created by Razib Chandra Deb on 11/23/15.
+//  Copyright © 2015 Razib Chandra Deb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DatabaseHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DatabaseHelper
+//
+//  Created by Razib Chandra Deb on 11/23/15.
+//  Copyright © 2015 Razib Chandra Deb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
